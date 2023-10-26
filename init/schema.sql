afdc93b8e6b704844bd1ce13d65c6bfe13d26b4c
Mateus <mateus@ufscar.br>
2023-10-26 10:02:03 -0300

    config: Adding schema.sql in project

---
+++ init/schema.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS `arch_clean`.`users`(
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    age INT NOT NULL,
+    PRIMARY KEY (id)
+);
