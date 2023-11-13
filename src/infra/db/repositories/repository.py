db2d3a1a1c1533f21b2563ce02d704cd40848cca
Mateus <mateus@ufscar.br>
2023-11-13 08:16:52 -0300

    config: Update the repository.py files in Project

--- src/infra/db/repositories/repository.py
+++ src/infra/db/repositories/repository.py
@@ -1,8 +1,12 @@
+from sqlalchemy import update
+from src.infra.db.settings.connection import DbConnectHandler as DB
+
+
 class UsersRepository:
 
     @classmethod
     def insert_user(cls, first_name: str, last_name: str, age: str) -> None:
-        with DBConnectHandler() as database:
+        with DB() as database:
             try:
                 new_registry = Users(
                     first_name = first_name,
@@ -17,7 +21,7 @@ class UsersRepository:
 
     @classmethod
     def select_user(cls, first_name: str) -> any:
-        with DbConnectHandler() as database:
+        with DB() as database:
             try:
                 users = (
                     database.session
@@ -31,15 +35,14 @@ class UsersRepository:
                 return exception
 
     @classmethod
-    def update_user(cls, first_name: str, last_name: str) -> any:
-        with DbConnectHandler() as database:
+    def update_user(cls, first_name: str, new_first_name: str) -> any:
+        with DB() as database:
             try:
                 users = (
-                    database.session
-                    .query(Users)
-                    .filter(Users.first_name == first_name)
-                    .all()
-                )
+                    update(Users)
+                    .where(Users.c.first_name == {first_name})
+                    .values(first_name = {new_first_name})
+                    )
                 return users
             except Exception as exception:
                 database.session.rollback()
@@ -47,7 +50,7 @@ class UsersRepository:
 
     @classmethod
     def delete_user(cls, id: int) -> None:
-        with DbConnectHandler() as database:
+        with DB() as database:
             try:
                 _find = database.session.get(User, id)
                 database.session.delete(_find)
