c1c73da8f3a7f03cafacfca52ae2e7f3d6dab2ab
Mateus <mateus@ufscar.br>
2023-11-13 14:09:20 -0300

    config: update test_repository.py file

---
+++ src/infra/db/repositories/test_repository.py
@@ -0,0 +1,50 @@
+from .repository import UsersRepository
+from sqlalchemy import text
+from src.infra.db.settings.connection import DbConnectHandler
+
+db_connect_handler = DbConnectHandler()
+engine = db_connect_handler.engine
+conn = engine.connect()
+
+def test_insert_user():
+    mocked_first_name = 'Marco'
+    mocked_last_name = 'Maria'
+    mocked_age = 40
+
+    user = UsersRepository()
+    user.insert_user(mocked_first_name, mocked_last_name, mocked_age)
+
+    sql = f'''
+        SELECT * FROM users
+        WHERE first_name = '{mocked_first_name}'
+        AND last_name = '{mocked_last_name}'
+        AND AGE = {mocked_age}
+    '''
+    response = conn.execute(text(sql))
+    registry = response.fetchall()[0]
+
+    assert registry.first_name == mocked_first_name
+    assert registry.last_name == mocked_last_name
+    assert registry.age == mocked_age
+
+
+def test_select_user():
+    mocked_first_name = 'Mateus'
+    mocked_last_name = 'Laranjeira'
+    mocked_age = 42
+
+    sql = f'''
+        INSERT INTO users (first_name, last_name, age)
+        VALUES ('{mocked_first_name}', '{mocked_last_name}', '{mocked_age}')
+    '''
+    conn.execute(text(sql))
+    conn.commit()
+
+    user = UsersRepository()
+    response = user.select_user(mocked_first_name)
+    for registry in response:
+        if registry.first_name == mocked_first_name:
+            assert registry.first_name == mocked_first_name
+            assert registry.last_name == mocked_last_name
+            assert registry.age == mocked_age
+            print(registry)
