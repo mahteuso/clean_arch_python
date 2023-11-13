0ffc215262324d8e23a7db29efb68cc934885f3c
Mateus <mateus@ufscar.br>
2023-11-13 07:47:10 -0300

    config: update repository.py file in project

--- src/infra/db/repositories/repository.py
+++ src/infra/db/repositories/repository.py
@@ -29,3 +29,29 @@ class UsersRepository:
             except Exception as exception:
                 database.session.rollback()
                 return exception
+
+    @classmethod
+    def update_user(cls, first_name: str, last_name: str) -> any:
+        with DbConnectHandler() as database:
+            try:
+                users = (
+                    database.session
+                    .query(Users)
+                    .filter(Users.first_name == first_name)
+                    .all()
+                )
+                return users
+            except Exception as exception:
+                database.session.rollback()
+                return exception
+
+    @classmethod
+    def delete_user(cls, id: int) -> None:
+        with DbConnectHandler() as database:
+            try:
+                _find = database.session.get(User, id)
+                database.session.delete(_find)
+                database.session.commit()
+            except Exception as exception:
+                database.session.rollback()
+                raise exception
