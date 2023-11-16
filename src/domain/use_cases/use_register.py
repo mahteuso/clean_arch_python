c57fd472971f774cd14e12bf70f7bcc7702a7dbe
Mateus <mateus@ufscar.br>
2023-11-16 08:21:24 -0300

    config: Adding user_registry.py in project

---
+++ src/domain/use_cases/use_register.py
@@ -0,0 +1,8 @@
+from abc import ABC, abstractmethod
+from typing import Dict
+
+class UserRegister(ABC):
+
+    @abstractmethod
+    def register(self, first_name: str, last_name: str, age: int) -> Dict:
+        ...
